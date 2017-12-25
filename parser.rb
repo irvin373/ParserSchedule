4734903c957a86d6c15a6a55e5a2862b21699169
irvin373 <irvin373@gmail.com>
2017-12-25 18:41:26 -0400

    parse

--- parser.rb
+++ parser.rb
@@ -1,3 +1,15 @@
+class Level
+	@name
+	@matters
+
+	def initialize(name, matters)
+		@name = name
+		@matters = matters
+	end
+
+	attr_accessor :name, :matters
+end
+
 class Matter
 	@code
 	@name
@@ -29,7 +41,7 @@ class Matter
 		begin
 		  temp = details.pop
 		  if /\d/.match(temp) == nil
-		  	@name += " " + temp
+		  	@name += " " + temp.strip
 		  end
 		end while /\d/.match(temp) == nil
 		@group = temp
@@ -37,7 +49,7 @@ class Matter
 		@schedule << details.pop
 		begin
 		  temp = details.pop
-		  @teacher += " " + temp
+		  @teacher += " " + temp.strip
 		end while details != []
 	end
 
@@ -54,6 +66,10 @@ class Matter
 
 	def ===(y)
 	   self == y && self.group == y.group
+	end
+
+	def to_json
+		return "{code: #{self.code}, name: #{self.name}, group: #{self.group}, teacher: #{self.teacher}, days: #{self.days}, schedule: #{self.schedule}}"
 	end
 
 	attr_accessor :code,:name,:group,:teacher,:schedule, :days
@@ -86,44 +102,74 @@ def separeLevels(file)
 	return levels
 end
 
-def getFist
+# def getFist
+# 	matter = []
+# 	file = File.new("419701.txt")
+# 	resp = clean(file)
+# 	levels = separeLevels(resp) 
+	
+# 	levels = levels.first
+	
+# 	levels.each do |level|
+# 		if /Plan:.*/.match(level).nil?  
+# 			level = level.sub("(*)","").rstrip
+# 			if level != ""
+# 				matter << Matter.new(level)
+# 			end
+# 		end
+# 	end
+# 	matter
+# end
+
+def getParseLevel(level)
 	matter = []
-	file = File.new("419701.txt")
-	resp = clean(file)
-	levels = separeLevels(resp) 
-	
-	levels = levels.first
-	
 	levels.each do |level|
 		if /Plan:.*/.match(level).nil?  
 			level = level.sub("(*)","").rstrip
 			if level != ""
 				matter << Matter.new(level)
 			end
+		else
+			label = split('   ').last.strip
 		end
 	end
-	matter
+	Level.new(label, matter)
 end
 
-def separateMatter(lines)
-	matterTemp = []
-	answer = []
-
-	first = lines.pop
-	while lines != []
-		matterTemp << first
-		iterator = lines.pop
-		while first == iterator
-			matterTemp << iterator
-			iterator = lines.pop
+def separateInMatter(lines)
+	hashMatter = Hash.new
+	hashGroup = Hash.new
+	lines.each do |matter|
+		if (hashMatter[matter.code].nil?)
+			hashGroup = Hash.new
+			hashGroup[matter.group] = matter
+			hashMatter[matter.code] = hashGroup
+		else
+			groups = hashMatter[matter.code]
+			if(groups[matter.group].nil?)
+				groups[matter.group] = matter
+			else
+				group = groups[matter.group]
+				group.days << matter.days.first
+				group.schedule << matter.schedule.first
+			end
 		end
-		first = iterator
-		answer << matterTemp
-		matterTemp = []
-	end 
-	answer
+	end
+	hashMatter
+end
+
+def parseSchedule
+	levelsParsed = []
+	file = File.new("419701.txt")
+	resp = clean(file)
+	levels = separeLevels(resp) 
+	levels.each do |level|
+		levelsParsed << getParseLevel(level)
+	end
+	levelsParsed
 end
 
 # puts getFist
-primer = separateMatter(getFist)
-puts primer
+# primer = separateInMatter(getFist)
+# puts primer
+# parseSchedule
